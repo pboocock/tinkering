aeba904b8635287f8f521625cb3cd3ca3cee9b77
Patrick Boocock <pboocock@fb.com>
2017-01-08 15:26:58 -0800

    First commit

--- weather_station.py
+++ weather_station.py
@@ -10,19 +10,13 @@ def millibars_to_in(p):
 
 # use moving average to smooth readings
 class TempManager:
-    def __init__(self, sense, n=10):
+    def __init__(self, sense, n=3):
         self.sense = sense
         self.temp_list = []
         self.n = n
 
-    def _smooth(self, t):
-        if len(self.temp_list) > self.n:
-            self.temp_list.pop()
-        self.temp_list.insert(0, t)
-        return sum(self.temp_list) / len(self.temp_list)
-
     @staticmethod
-    def _cpu_temp():
+    def cpu_temp():
         return float(
             os.popen('vcgencmd measure_temp').readline().replace(
                 "temp=", ""
@@ -30,8 +24,14 @@ class TempManager:
         )
 
     @staticmethod
-    def _c_to_f(t):
+    def c_to_f(t):
         return (t * 1.8) + 32
+
+    def _smooth(self, t):
+        self.temp_list.insert(0, t)
+        if len(self.temp_list) > self.n:
+            self.temp_list.pop()
+        return sum(self.temp_list) / len(self.temp_list)
 
     def get_temp(self, c=False):
         t = (
@@ -39,29 +39,29 @@ class TempManager:
                 self.sense.get_temperature_from_pressure()
             ) / 2
         t_corr = t - (
-            (self._cpu_temp() - t) / 1.5
+            (self.cpu_temp() - t) / 1.5
         )
         if c:
             return self._smooth(t_corr)
         else:
-            return self._c_to_f(self._smooth(t_corr))
+            return self.c_to_f(self._smooth(t_corr))
 
 
 def main():
     sense = SenseHat()
     temp_manager = TempManager(sense)
-    # event = sense.stick.wait_for_event()
-    # if event.direction == 'push':
-    sense.show_message(
-        "Temp: {t} Humidity: {h} Pressure {p}".format(
-            t=round(temp_manager.get_temp(), 0),
-            h=round(sense.get_humidity(), 0),
-            p=round(millibars_to_in(sense.get_pressure()), 2)
+    sense.show_message("Starting up", text_colour=[255, 255, 0], back_colour=[0, 0, 255])
+    while True:
+        # event = sense.stick.wait_for_event()
+        # if event.direction == 'push':
+        sense.show_message(
+            "Temp: {t} Humidity: {h} Pressure {p}".format(
+                t=round(temp_manager.get_temp(), 0),
+                h=round(sense.get_humidity(), 0),
+                p=round(millibars_to_in(sense.get_pressure()), 2)
+            )
         )
-    )
 
 
 if __name__ == '__main__':
-    while True:
-        main()
-        time.sleep(30)
+    main()
